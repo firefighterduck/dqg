0396255388193f9e40f4f13e7259206330d8e247
Florian Sextl <sextl@in.tum.de>
2021-05-01 13:10:11 +0200

    add graph parsing

---
+++ src/graph.rs
@@ -0,0 +1,306 @@
+use std::{os::raw::c_int, usize};
+
+use nauty_Traces_sys::{empty_graph, ADDONEARC, SETWORDSNEEDED};
+
+pub type Colour = c_int;
+pub type VertexIndex = c_int;
+
+#[derive(Debug, PartialEq, Eq)]
+enum GraphState {
+    IndexOrdered,
+    ColourGrouped,
+    ColourGroupedOrdered,
+    Chaos,
+}
+
+#[derive(Debug)]
+pub struct Graph {
+    vertices: Vec<Vertex>,
+    state: GraphState,
+    keep_state_auto: bool,
+}
+
+#[derive(Debug, Clone)]
+pub struct Vertex {
+    pub index: VertexIndex,
+    pub edges_to: Vec<c_int>,
+    pub colour: Colour,
+}
+
+#[derive(Debug)]
+pub struct NautyGraph {
+    /// actual graph
+    pub adjacency_matrix: Vec<u64>,
+    /// lab
+    pub vertex_order: Vec<VertexIndex>,
+    /// ptn aka the colouring
+    pub partition: Vec<VertexIndex>,
+}
+
+impl Graph {
+    pub fn new_empty(n: usize) -> Self {
+        Graph {
+            vertices: Vec::with_capacity(n),
+            state: GraphState::Chaos,
+            keep_state_auto: true,
+        }
+    }
+
+    pub fn size(&self) -> usize {
+        self.vertices.len()
+    }
+
+    pub fn new_ordered(n: usize) -> Self {
+        let mut vertices = Vec::with_capacity(n);
+        for index in 0..n {
+            vertices.push(Vertex::new(index as VertexIndex, 0));
+        }
+        Graph {
+            vertices,
+            state: GraphState::IndexOrdered,
+            keep_state_auto: true,
+        }
+    }
+
+    pub fn from_colour_list(coloured_vertices: &[Colour]) -> Self {
+        let mut vertices = Vec::with_capacity(coloured_vertices.len());
+        for (index, colour) in coloured_vertices.iter().enumerate() {
+            vertices.push(Vertex::new(index as VertexIndex, *colour));
+        }
+        Graph {
+            vertices,
+            state: GraphState::Chaos,
+            keep_state_auto: true,
+        }
+    }
+
+    pub fn add_vertex(&mut self, vertex: Vertex) {
+        use GraphState::*;
+        match self.state {
+            IndexOrdered => {
+                let index = vertex.index as usize;
+                if let Some(old_vertex) = self.vertices.get_mut(index) {
+                    *old_vertex = vertex;
+                } else {
+                    if self.vertices.len() < index {
+                        self.vertices.reserve(index - self.vertices.len() + 1);
+                    }
+                    self.vertices[index] = vertex;
+                }
+            }
+            _ => {
+                self.vertices.push(vertex);
+                self.state = Chaos;
+            }
+        }
+    }
+
+    fn get_vertex(&mut self, index: VertexIndex) -> Option<&Vertex> {
+        match self.state {
+            GraphState::IndexOrdered => self.vertices.get(index as usize),
+            _ => self.vertices.iter().find(|vertex| vertex.index == index),
+        }
+    }
+
+    fn get_vertex_mut(&mut self, index: VertexIndex) -> Option<&mut Vertex> {
+        match self.state {
+            GraphState::IndexOrdered => self.vertices.get_mut(index as usize),
+            _ => self
+                .vertices
+                .iter_mut()
+                .find(|vertex| vertex.index == index),
+        }
+    }
+
+    pub fn add_arc(&mut self, start: VertexIndex, end: VertexIndex) {
+        if let Some(start_vertex) = self.get_vertex_mut(start) {
+            start_vertex.add_edge(end);
+        } else {
+            let mut vertex = Vertex::new(start, 0);
+            vertex.add_edge(end);
+            self.add_vertex(vertex);
+        }
+    }
+
+    pub fn add_edge(&mut self, start: VertexIndex, end: VertexIndex) {
+        self.add_arc(start, end);
+        self.add_arc(end, start);
+    }
+
+    pub fn set_colours(&mut self, colours: &[Colour]) {
+        for (index, colour) in colours.iter().enumerate() {
+            if let Some(vertex) = self.get_vertex_mut(index as VertexIndex) {
+                vertex.colour = *colour;
+            } else {
+                self.add_vertex(Vertex::new(index as VertexIndex, *colour));
+            }
+        }
+
+        self.keep_state_auto = false;
+        self.state = GraphState::ColourGroupedOrdered;
+    }
+
+    pub fn order(&mut self, order: &[VertexIndex]) {
+        let mut ordered_vertices = Vec::with_capacity(self.vertices.len());
+        for index in order {
+            if let Some(vertex) = self.get_vertex(*index) {
+                ordered_vertices.push(vertex.clone());
+            } else {
+                ordered_vertices.push(Vertex::new(*index, 0));
+            }
+        }
+
+        self.vertices = ordered_vertices;
+        self.keep_state_auto = false;
+        self.state = GraphState::IndexOrdered;
+    }
+
+    pub fn group_colours(&mut self) {
+        use GraphState::*;
+        match self.state {
+            IndexOrdered => {
+                self.vertices.sort_by(|a, b| a.colour.cmp(&b.colour));
+                self.state = ColourGroupedOrdered;
+            }
+            Chaos => {
+                self.vertices
+                    .sort_unstable_by(|a, b| a.colour.cmp(&b.colour));
+                self.state = ColourGrouped;
+            }
+            _ => (),
+        }
+    }
+
+    pub fn sort(&mut self) {
+        if self.state != GraphState::IndexOrdered {
+            self.vertices.sort_unstable_by(|a, b| a.index.cmp(&b.index));
+            self.state = GraphState::IndexOrdered;
+        }
+    }
+
+    pub fn prepare_nauty(&mut self) -> NautyGraph {
+        let n = self.vertices.len();
+        let m = SETWORDSNEEDED(n);
+
+        let mut nauty_graph = NautyGraph {
+            adjacency_matrix: empty_graph(m, n),
+            vertex_order: Vec::with_capacity(n),
+            partition: Vec::with_capacity(n),
+        };
+
+        if self.keep_state_auto {
+            self.sort();
+            self.group_colours();
+        }
+
+        for vertex in self.vertices.iter() {
+            nauty_graph.vertex_order.push(vertex.index);
+
+            nauty_graph.partition.push(vertex.colour);
+
+            for end in vertex.edges_to.iter() {
+                ADDONEARC(
+                    &mut nauty_graph.adjacency_matrix,
+                    vertex.index as usize,
+                    *end as usize,
+                    m,
+                );
+            }
+        }
+
+        let mut last_colour = -1;
+        for colour in nauty_graph.partition.iter_mut().rev() {
+            if *colour != last_colour {
+                last_colour = *colour;
+                *colour = 0;
+            } else {
+                *colour = 1;
+            }
+        }
+
+        nauty_graph
+    }
+}
+
+impl Vertex {
+    pub fn new(index: VertexIndex, colour: Colour) -> Self {
+        Vertex {
+            index,
+            edges_to: Vec::new(),
+            colour,
+        }
+    }
+
+    pub fn add_edge(&mut self, end: VertexIndex) {
+        self.edges_to.push(end);
+    }
+}
+
+impl NautyGraph {
+    pub fn check_valid(&self) -> bool {
+        let n = self.partition.len();
+        let m = SETWORDSNEEDED(n);
+
+        self.adjacency_matrix.len() == n * m && self.vertex_order.len() == n
+    }
+
+    pub fn graph_repr_sizes(&self) -> (usize, usize) {
+        let n = self.partition.len();
+        (n, SETWORDSNEEDED(n))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use nauty_Traces_sys::{densenauty, optionblk, statsblk, FALSE, TRUE};
+
+    use super::*;
+
+    #[test]
+    fn correct_nauty_repr() {
+        let mut graph = Graph::new_empty(8);
+        graph.add_edge(0, 1);
+        graph.add_edge(0, 3);
+        graph.add_edge(0, 4);
+        graph.add_edge(1, 2);
+        graph.add_edge(1, 5);
+        graph.add_edge(2, 3);
+        graph.add_edge(2, 6);
+        graph.add_edge(3, 7);
+        graph.add_edge(4, 5);
+        graph.add_edge(4, 7);
+        graph.add_edge(5, 6);
+        graph.add_edge(6, 7);
+
+        let order = [2, 0, 1, 3, 4, 5, 6, 7];
+        let colours = [1, 2, 2, 2, 2, 2, 2, 2];
+        graph.order(&order);
+        graph.set_colours(&colours);
+
+        let mut nauty_graph = graph.prepare_nauty();
+        assert_eq!(nauty_graph.vertex_order, order);
+        assert_eq!(nauty_graph.partition, [0, 1, 1, 1, 1, 1, 1, 0]);
+
+        let mut options = optionblk::default();
+        options.writeautoms = TRUE;
+        options.defaultptn = FALSE;
+        let mut stats = statsblk::default();
+        let mut orbits = vec![0; 8];
+
+        unsafe {
+            densenauty(
+                nauty_graph.adjacency_matrix.as_mut_ptr(),
+                nauty_graph.vertex_order.as_mut_ptr(),
+                nauty_graph.partition.as_mut_ptr(),
+                orbits.as_mut_ptr(),
+                &mut options,
+                &mut stats,
+                1,
+                8,
+                std::ptr::null_mut(),
+            );
+        }
+
+        assert_eq!(orbits, [0, 1, 2, 1, 4, 0, 1, 0]);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,111 @@
-fn main() {
-    println!("Hello, world!");
+mod graph;
+use std::{
+    error::Error,
+    io::{self, Stdin, Write},
+    os::raw::c_int,
+    usize,
+};
+
+use graph::*;
+use nauty_Traces_sys::{densenauty, optionblk, statsblk};
+
+fn read_graph(stdin: &Stdin) -> Result<Graph, io::Error> {
+    let mut buffer = String::new();
+    let number_of_vertices;
+
+    println!("Input graph size:");
+    loop {
+        print!("n=");
+        io::stdout().flush()?;
+        stdin.read_line(&mut buffer)?;
+
+        match buffer.trim().parse::<usize>() {
+            Ok(n) => {
+                number_of_vertices = n;
+                break;
+            }
+            Err(_) => println!("Please insert only natural numbers in decimal!"),
+        }
+
+        buffer.clear();
+    }
+
+    Ok(Graph::new_ordered(number_of_vertices))
+}
+
+fn read_vertex(index: VertexIndex, graph: &mut Graph, stdin: &Stdin) -> Result<bool, io::Error> {
+    let mut line_buffer = String::new();
+    let mut should_continue = true;
+
+    'input: loop {
+        print!("Edges from {}: ", index);
+        io::stdout().flush()?;
+
+        line_buffer.clear();
+        stdin.read_line(&mut line_buffer)?;
+        for input_part in line_buffer.split_whitespace() {
+            if let Ok(end) = input_part.parse::<VertexIndex>() {
+                if end < graph.size() as i32 {
+                    graph.add_edge(index, end);
+                } else {
+                    println!(
+                        "Please only input valid vertex indices (i.e. between 0 and {})!",
+                        graph.size()
+                    );
+                    continue 'input;
+                }
+            } else if input_part.chars().next().unwrap_or(' ') == ';' {
+                break 'input;
+            } else if input_part.chars().next().unwrap_or(' ') == '.' {
+                should_continue = false;
+                break 'input;
+            } else {
+                break;
+            }
+        }
+
+        println!("Please insert the edges from vertex {} in this format: `Edges from {}: #1 #2 #3 (;|.)` where \
+#i is the vertex number of the end node of an edge from this vertex. Also end the line with a `;` to continue \
+with the next vertex or a `.` to end inputting edges.", index, index);
+    }
+
+    Ok(should_continue)
+}
+
+fn call_nauty(mut nauty_graph: NautyGraph) {
+    let (n, m) = nauty_graph.graph_repr_sizes();
+    let mut options = optionblk::default();
+    let mut stats = statsblk::default();
+    let mut orbits = vec![0 as c_int; n];
+
+    unsafe {
+        densenauty(
+            nauty_graph.adjacency_matrix.as_mut_ptr(),
+            nauty_graph.vertex_order.as_mut_ptr(),
+            nauty_graph.partition.as_mut_ptr(),
+            orbits.as_mut_ptr(),
+            &mut options,
+            &mut stats,
+            m as c_int,
+            n as c_int,
+            std::ptr::null_mut(),
+        );
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let stdin = io::stdin();
+    let mut graph = read_graph(&stdin)?;
+
+    for i in 0..graph.size() {
+        if !read_vertex(i as VertexIndex, &mut graph, &stdin)? {
+            break;
+        }
+    }
+
+    let nauty_graph = graph.prepare_nauty();
+    assert!(nauty_graph.check_valid());
+    call_nauty(nauty_graph);
+
+    Ok(())
 }
