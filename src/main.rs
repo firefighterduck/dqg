9c20efe09834be821336766941a33dbc3ecc1213
Florian Sextl <sextl@in.tum.de>
2021-05-01 14:27:12 +0200

    use libffi for better callbacks

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ use std::{
     error::Error,
     io::{self, Stdin, Write},
     os::raw::c_int,
+    slice::from_raw_parts,
     usize,
 };
 
 use graph::*;
+use libffi::high::ClosureMut6;
 use nauty_Traces_sys::{densenauty, optionblk, statsblk};
 
 fn read_graph(stdin: &Stdin) -> Result<Graph, io::Error> {
@@ -72,25 +74,43 @@ with the next vertex or a `.` to end inputting edges.", index, index);
     Ok(should_continue)
 }
 
-fn call_nauty(mut nauty_graph: NautyGraph) {
+fn call_nauty(mut nauty_graph: NautyGraph) -> Vec<Vec<c_int>> {
     let (n, m) = nauty_graph.graph_repr_sizes();
-    let mut options = optionblk::default();
     let mut stats = statsblk::default();
     let mut orbits = vec![0 as c_int; n];
+    let mut options = optionblk::default();
+    let mut generators = Vec::new();
+    // Limit how long the closure can reference generators so that we can return it afterwards.
+    {
+        let mut userautomproc =
+            |_count, generator_ptr: *mut c_int, _orbits, _numorbits, _stabvertex, n: c_int| {
+                let mut generator = Vec::with_capacity(n as usize);
+                let generator_raw = unsafe { from_raw_parts(generator_ptr, n as usize) };
 
-    unsafe {
-        densenauty(
-            nauty_graph.adjacency_matrix.as_mut_ptr(),
-            nauty_graph.vertex_order.as_mut_ptr(),
-            nauty_graph.partition.as_mut_ptr(),
-            orbits.as_mut_ptr(),
-            &mut options,
-            &mut stats,
-            m as c_int,
-            n as c_int,
-            std::ptr::null_mut(),
-        );
+                for vertex in generator_raw {
+                    generator.push(*vertex);
+                }
+
+                generators.push(generator);
+            };
+        let userautomproc = ClosureMut6::new(&mut userautomproc);
+        options.userautomproc = Some(*userautomproc.code_ptr());
+
+        unsafe {
+            densenauty(
+                nauty_graph.adjacency_matrix.as_mut_ptr(),
+                nauty_graph.vertex_order.as_mut_ptr(),
+                nauty_graph.partition.as_mut_ptr(),
+                orbits.as_mut_ptr(),
+                &mut options,
+                &mut stats,
+                m as c_int,
+                n as c_int,
+                std::ptr::null_mut(),
+            );
+        }
     }
+    generators
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -105,7 +125,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let nauty_graph = graph.prepare_nauty();
     assert!(nauty_graph.check_valid());
-    call_nauty(nauty_graph);
+    let generators = call_nauty(nauty_graph);
+
+    for generator in generators {
+        println!("{:?}", generator);
+    }
 
     Ok(())
 }
